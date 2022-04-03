6e68faa776d5d25ce65fbb6ac3aa9a5e444aee45
DenisDmitrienko <DVDmitrienko@sberbank.ru>
2022-04-03 18:35:37 +0300

    minimal compose application

---
+++ app/src/test/java/p/l/e/x/u/s/interactive_fiction/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package p.l.e.x.u.s.interactive_fiction
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
